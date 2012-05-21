7062d65036c4fd31eb737d33ddb381a02fc6289f
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-05-21 12:12:21 -0400

    Fix issue with default text fallbacks were not working in some cases.
    Simple issue with if logic. Fixed and further cleanup/refactoring
    done while at it.

--- mod.seo.php
+++ mod.seo.php
@@ -72,6 +72,14 @@ class Seo {
 		return $entry_id;
 	}
 
+	protected function _defaultValue($type) {
+		if (isset($this->options["use_default_$type"]) && $this->options["use_default_$type"] == 'yes') {
+			return $this->options["default_$type"];
+		} else {
+			return '';
+		}
+	}
+
 	function title() {
 		//Get entry_id first
 		$entry_id = $this->_getEntryID();
@@ -85,65 +93,57 @@ class Seo {
 			$sql = "SELECT `title` FROM `exp_seo_data` WHERE `entry_id` = ? AND `site_id` = ?;";
 
 			$res = $this->EE->db->query($sql, array($entry_id, $this->site_id));
+			$title = '';
 			if ($res->num_rows() > 0) {
-				if (!empty($prepend)) {
-					$final_prepend = $prepend;
-				} else {
-					$final_prepend = $this->options['prepend_to_title'];
-				}
-
-				if (!empty($append)) {
-					$final_append = $append;
-				} else {
-					$final_append = $this->options['append_to_title'];
-				}
-
-				return $this->return_data = $final_prepend.($res->row('title')).$final_append; //removed htmlentities()
+				$title = $res->row('title');
 			}
+
+			$this->return_data = ($title != '') ? ($res->row('title')) : $this->_defaultValue('title');
 		} else {
-			//Fallback
-			if ($fallback != FALSE && $fallback != '') {
-				if (!empty($prepend)) {
-					$final_prepend = $prepend;
-				} else {
-					$final_prepend = $this->options['prepend_to_title'];
-				}
-
-				if (!empty($append)) {
-					$final_append = $append;
-				} else {
-					$final_append = $this->options['append_to_title'];
-				}
-				return $this->return_data = $final_prepend.$fallback.$final_append;
-			}
-
-			if (isset($this->options['use_default_title']) && $this->options['use_default_title'] == 'yes') {
-				return $this->return_data = $this->options['default_title'];
-			} else {
-				return '';
-			}
+			//Manual fallback
+			$this->return_data = $fallback;
 		}
+
+		//Add in prepend/append
+		$final_prepend = '';
+		if (!empty($prepend)) {
+			$final_prepend = $prepend;
+		} else {
+			$final_prepend = $this->options['prepend_to_title'];
+		}
+
+		$final_append = '';
+		if (!empty($append)) {
+			$final_append = $append;
+		} else {
+			$final_append = $this->options['append_to_title'];
+		}
+
+		$this->return_data = $final_prepend.$this->return_data.$final_append;
+
+		return $this->return_data;
 	}
 
 	function description() {
 		//Get entry_id first.
 		$entry_id = $this->_getEntryID();
+
+		//Go ahead and get the description
 		if (!empty($entry_id)) {
-			//Go ahead and get the description
 			$sql = "SELECT `description` FROM `exp_seo_data` WHERE `entry_id` = ? AND `site_id` = ?;";
 
 			$res = $this->EE->db->query($sql, array($entry_id, $this->site_id));
+			$description = '';
 			if ($res->num_rows() > 0) {
-				return $this->return_data = ($res->row('description'));	//removed htmlentities()
+				$description = $res->row('description');
 			}
+
+			$this->return_data = ($description != '') ? $description: $this->_defaultValue('description');
 		} else {
-			//Fallback to default
-			if (isset($this->options['use_default_description']) && $this->options['use_default_description'] == 'yes') {
-				return $this->return_data = $this->options['default_description'];
-			} else {
-				return '';
-			}
+			$this->return_data = $this->_defaultValue('description');
 		}
+
+		return $this->return_data;
 	}
 
 	function keywords() {
@@ -154,17 +154,18 @@ class Seo {
 			$sql = "SELECT `keywords` FROM `exp_seo_data` WHERE `entry_id` = ? AND `site_id` = ?;";
 
 			$res = $this->EE->db->query($sql, array($entry_id, $this->site_id));
+			$keywords = '';
 			if ($res->num_rows() > 0) {
-				return $this->return_data = ($res->row('keywords'));	//removed htmlentities()
+				$keywords = $res->row('keywords');
 			}
+
+			$this->return_data = ($keywords != '') ? $keywords : $this->_defaultValue('keywords');
 		} else {
 			//Fallback to default
-			if (isset($this->options['use_default_keywords']) && $this->options['use_default_keywords'] == 'yes') {
-				return $this->return_data = $this->options['default_keywords'];
-			} else {
-				return '';
-			}
+			$this->return_data = $this->_defaultValue('keywords');
 		}
+
+		return $this->return_data;
 	}
 
 	function canonical() {
@@ -172,14 +173,17 @@ class Seo {
 
 		if (empty($url)) {
 			$this->return_data = '';
-			return '';
+		} else {
+			$this->return_data = '<link rel="canonical" href="'.$url.'" />';
 		}
 
-		return $this->return_data = '<link rel="canonical" href="'.$url.'" />';
+		return $this->return_data;
 	}
 
 	function privacy() {
-		if (empty($this->options['robots'])) { $this->options['robots'] = $this->defaults['robots']; }
+		if (empty($this->options['robots'])) {
+			$this->options['robots'] = $this->defaults['robots'];
+		}
 		return $this->return_data = '<meta name="robots" content="'.$this->options['robots'].'" />';
 	}
 }
